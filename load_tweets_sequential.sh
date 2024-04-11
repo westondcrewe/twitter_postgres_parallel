dc62841999a4819a05233418df5c65213ed6687d
westondcrewe <westondcrewe@gmail.com>
2024-04-11 12:09:02 -0700

    ports error

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -8,7 +8,7 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:2000 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1318 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
@@ -17,12 +17,12 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
-    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:2001 --inputs "$file"
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:2000 --inputs "$file"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:2001/ --inputs $file
 done
