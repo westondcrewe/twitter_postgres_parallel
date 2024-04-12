e938d52e8278b86157371053906310dbc9da9b6e
westondcrewe <westondcrewe@gmail.com>
2024-04-11 17:22:16 -0700

    checkpoint

--- README.md
+++ README.md
@@ -280,9 +280,9 @@ Ensure that your runtimes on the lambda server are recorded below.
 
 |                        | elapsed time (sequential) | elapsed time (parallel)   |
 | -----------------------| ------------------------- | ------------------------- |
-| `pg_normalized`        |                           |                           | 
-| `pg_normalized_batch`  |                           |                           | 
-| `pg_denormalized`      |                           |                           | 
+| `pg_normalized`        |         8m21.668s         |                           | 
+| `pg_normalized_batch`  |         2m52.303s         |                           | 
+| `pg_denormalized`      |         0m20.829s         |                           | 
 
 Then upload a link to your forked github repo on sakai.
 
--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,23 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time for file in $files; do
+	echo 
+	time echo "$files" | parallel sh load_denormalized.sh
+done;
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time for file in $files; do
+	echo
+done;
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time for file in $files; do
+	echo
+done;
